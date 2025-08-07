6844f768d7680e9b1b891ebe771348eed9b7f3c8
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-08-07 20:32:00 -0300

    config: add browse router and chakra provider in application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    ),
+    children: [
+      
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
